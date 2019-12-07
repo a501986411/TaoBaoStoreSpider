26d049d60c1269201da7392bfeb37ea1c54e10ed
a501986411 <501986411@qq.com>
2019-12-07 12:37:56 +0800

    修改配置文件

--- conf.py
+++ conf.py
@@ -1,8 +1,8 @@
 # -*- coding: utf-8 -*-
 
 class conf:
-    conf_dir = "D:\\project\\TaobaoStoreSpider\\config\\"
-    # conf_dir = "/www/TaobaoStoreSpider/config/"
+    # conf_dir = "D:\\project\\TaobaoStoreSpider\\config\\"
+    conf_dir = "/www/TaoBaoStoreSpider/config/"
     def get_db_conf(self):
         return self.conf_dir+"db.ini"
 
